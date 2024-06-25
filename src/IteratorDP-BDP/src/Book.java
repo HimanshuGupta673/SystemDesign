948af4bf4cd236d28b74019ccc3550a20e9187e4
Himanshu Gupta <112652867+HimanshuGupta673@users.noreply.github.com>
2024-06-25 18:50:31 +0530

    Iterator Design Pattern-BDP

---
+++ src/IteratorDP-BDP/src/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private int price;
+    private String bookName;
+
+    Book(int price, String bookName) {
+        this.price = price;
+        this.bookName = bookName;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public String getBookName() {
+        return bookName;
+    }
+}
